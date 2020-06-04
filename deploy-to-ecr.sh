ec26844350b45ca0ca4a9df2e07e8a8f37b4e40a
ozer <ozer.cevikaslan@gmail.com>
2020-06-05 01:14:35 +1000

    Add GitHub Actions.

--- deploy-to-ecr.sh
+++
@@ -1,4 +0,0 @@
-aws ecr get-login --no-include-email --region eu-central-1 | /bin/bash
-docker build -t coffee-shop-api .
-docker tag coffee-shop-api:latest 726120415058.dkr.ecr.eu-central-1.amazonaws.com/coffee-shop-api:latest
-docker push 726120415058.dkr.ecr.eu-central-1.amazonaws.com/coffee-shop-api:latest
